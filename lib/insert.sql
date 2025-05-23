497f94421f4a92cfbb44ab71a62dffb2e7b51a9e
{JosephNgugi254} <{solitaryjoe069@gmail.com}>
2025-05-23 12:05:21 +0300

    added sql queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 5, 'M', 'brown', 'calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'M', 'black', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 4, 'F', 'white', 'calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 7, 'M', 'brown', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 3, 'F', 'black', 'calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 8, 'M', 'green', 'aggressive', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 2, 'F', 'white', 'calm', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 0, 'M', 'black', 'aggressive', true);
